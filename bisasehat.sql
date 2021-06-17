972c174b57ff287ac02d30051bb21366db57b51f
abdilaziez <72601253+abdilaziez@users.noreply.github.com>
2021-06-17 11:33:18 +0700

    update

--- bisasehat.sql
+++ bisasehat.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2020 at 12:18 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.2.12
+-- Generation Time: Jun 17, 2021 at 06:32 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `fightcovid`
+-- Database: `bisasehat`
 --
 
 -- --------------------------------------------------------
@@ -45,10 +44,10 @@ INSERT INTO `admin` (`id_admin`, `username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `berita`
+-- Table structure for table `artikelkesehatan`
 --
 
-CREATE TABLE `berita` (
+CREATE TABLE `artikelkesehatan` (
   `id_berita` int(11) NOT NULL,
   `judulBerita` varchar(100) NOT NULL,
   `tanggalPosting` date NOT NULL,
@@ -56,6 +55,14 @@ CREATE TABLE `berita` (
   `deskripsi` longtext NOT NULL,
   `link` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `artikelkesehatan`
+--
+
+INSERT INTO `artikelkesehatan` (`id_berita`, `judulBerita`, `tanggalPosting`, `gambar`, `deskripsi`, `link`) VALUES
+(2, 'trending covid bulan ini', '2021-06-17', '60ca109c038ed.jpg', 'Berita Terbaru pandemi covid-19 ', 'https://www.halodoc.com/cari-dokter'),
+(5, 'trending covid bulan ini', '2021-06-09', '60cac3f4cae7c.jpg', 'asalsajskajksaj\r\nqweqweqweqwe\r\nqweqweqwe', 'https://www.halodoc.com/cari-dokter');
 
 -- --------------------------------------------------------
 
@@ -67,32 +74,8 @@ CREATE TABLE `diskusi` (
   `id_diskusi` int(11) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `nomerHp` varchar(200) NOT NULL,
   `komentar` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `gejala`
---
-
-CREATE TABLE `gejala` (
-  `id_gejala` int(11) NOT NULL,
-  `gejala` varchar(100) NOT NULL,
-  `keterangan` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `jenispasien`
---
-
-CREATE TABLE `jenispasien` (
-  `id_jenis` int(11) NOT NULL,
-  `jenisPasien` varchar(110) NOT NULL,
-  `keterangan` longtext NOT NULL,
-  `gambar` varchar(110) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -105,41 +88,39 @@ CREATE TABLE `pesan` (
   `id_pesan` int(11) NOT NULL,
   `namaLengkap` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `subjek` varchar(200) NOT NULL,
+  `nomerHp` varchar(200) NOT NULL,
   `keterangan` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `pesan`
+--
+
+INSERT INTO `pesan` (`id_pesan`, `namaLengkap`, `email`, `nomerHp`, `keterangan`) VALUES
+(2, 'Abdil Aziez Al Mekhani', 'abdilaziez81086@gmail.com', '', 'kakldjlasjdjsljdlkqjwjlqjewjqlkejklqjwe'),
+(3, 'Abdil Aziez Al Mekhani', 'komdis@ilkomunej.com', '', 'ksakjsakjsjajksjkajksajs'),
+(4, 'Abdil Aziez Al Mekhani', 'siroontong@yahoo.com', '08999072000', 'jhkjhkjqhwkjhkehqwhekhqhwkjehqwehhqwheqwe');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rs`
+-- Table structure for table `solusipenyakit`
 --
 
-CREATE TABLE `rs` (
-  `id_rs` int(11) NOT NULL,
-  `namaRumahSakit` varchar(100) NOT NULL,
-  `daerah` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `updatecovid`
---
-
-CREATE TABLE `updatecovid` (
-  `id_update` int(11) NOT NULL,
-  `positif` int(11) NOT NULL,
-  `pdp` int(11) NOT NULL,
-  `odp` int(11) NOT NULL
+CREATE TABLE `solusipenyakit` (
+  `id_jenis` int(11) NOT NULL,
+  `jenisPasien` varchar(110) NOT NULL,
+  `keterangan` longtext NOT NULL,
+  `gambar` varchar(110) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `updatecovid`
+-- Dumping data for table `solusipenyakit`
 --
 
-INSERT INTO `updatecovid` (`id_update`, `positif`, `pdp`, `odp`) VALUES
-(2, 0, 0, 0);
+INSERT INTO `solusipenyakit` (`id_jenis`, `jenisPasien`, `keterangan`, `gambar`) VALUES
+(2, 'Corona', '1. Mandi\r\n2. Cuci Baju\r\n3. Cuci Tangan', '60ca0e559ce69.jpg'),
+(4, 'Kanker', 'asasasa\r\nasasasa\r\nasasasa\r\nasasas', '60cac3cc94c74.jpg');
 
 --
 -- Indexes for dumped tables
@@ -152,9 +133,9 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id_admin`);
 
 --
--- Indexes for table `berita`
+-- Indexes for table `artikelkesehatan`
 --
-ALTER TABLE `berita`
+ALTER TABLE `artikelkesehatan`
   ADD PRIMARY KEY (`id_berita`);
 
 --
@@ -164,34 +145,16 @@ ALTER TABLE `diskusi`
   ADD PRIMARY KEY (`id_diskusi`);
 
 --
--- Indexes for table `gejala`
---
-ALTER TABLE `gejala`
-  ADD PRIMARY KEY (`id_gejala`);
-
---
--- Indexes for table `jenispasien`
---
-ALTER TABLE `jenispasien`
-  ADD PRIMARY KEY (`id_jenis`);
-
---
 -- Indexes for table `pesan`
 --
 ALTER TABLE `pesan`
   ADD PRIMARY KEY (`id_pesan`);
 
 --
--- Indexes for table `rs`
+-- Indexes for table `solusipenyakit`
 --
-ALTER TABLE `rs`
-  ADD PRIMARY KEY (`id_rs`);
-
---
--- Indexes for table `updatecovid`
---
-ALTER TABLE `updatecovid`
-  ADD PRIMARY KEY (`id_update`);
+ALTER TABLE `solusipenyakit`
+  ADD PRIMARY KEY (`id_jenis`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -204,10 +167,10 @@ ALTER TABLE `admin`
   MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `berita`
+-- AUTO_INCREMENT for table `artikelkesehatan`
 --
-ALTER TABLE `berita`
-  MODIFY `id_berita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `artikelkesehatan`
+  MODIFY `id_berita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `diskusi`
@@ -216,34 +179,16 @@ ALTER TABLE `diskusi`
   MODIFY `id_diskusi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `gejala`
---
-ALTER TABLE `gejala`
-  MODIFY `id_gejala` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `jenispasien`
---
-ALTER TABLE `jenispasien`
-  MODIFY `id_jenis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT for table `pesan`
 --
 ALTER TABLE `pesan`
-  MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `rs`
+-- AUTO_INCREMENT for table `solusipenyakit`
 --
-ALTER TABLE `rs`
-  MODIFY `id_rs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `updatecovid`
---
-ALTER TABLE `updatecovid`
-  MODIFY `id_update` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `solusipenyakit`
+  MODIFY `id_jenis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
